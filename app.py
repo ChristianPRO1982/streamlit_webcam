e151c2a1be402ed40ee743bba1ed76188b94d0e8
Christian Martiny <christian.martiny@outlook.com>
2024-07-02 15:49:10 +0200

    test 2

--- app.py
+++ app.py
@@ -1,34 +1,3 @@
 import streamlit as st
-import cv2
-import numpy as np
 
-def main():
-    st.title("Application de Webcam avec Streamlit")
-
-    # Démarrer la webcam
-    cap = cv2.VideoCapture(0)
-
-    stframe = st.empty()
-
-    while True:
-        ret, frame = cap.read()
-        print(ret)
-        print(frame)
-        if not ret:
-            st.write("Impossible d'accéder à la webcam.")
-            break
-
-        # Convertir l'image de BGR à RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Afficher l'image sur la page web
-        stframe.image(frame, channels='RGB')
-
-        # Arrêter la webcam si le bouton est cliqué
-        # if st.button('Arrêter la webcam'):
-        #     break
-
-    cap.release()
-
-if __name__ == "__main__":
-    main()
+st.camera_input("WEBCAM")
